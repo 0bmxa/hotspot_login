6f265648c57ccc34cb75ac5b3fda790f93342f3f
0bmxa <0bmxa@users.noreply.github.com>
2018-10-07 16:19:55 +0200

    BooksAndBagels always on

--- strategies/BooksAndBagelsLoginStrategy.py
+++ strategies/BooksAndBagelsLoginStrategy.py
@@ -8,10 +8,11 @@ class BooksAndBagelsLoginStrategy(MeinHotspotLoginStrategy):
         return "BooksAndBagels"
 
     def check_preconditions(self):
-        # Fine Bagels Hotspot on weekends:
-        # Saturday only from 17:00, Sunday from 19:00 on
-        now = datetime.datetime.now()
-        if (now.weekday() == 5 and now.hour < 17) or \
-           (now.weekday() == 6 and now.hour < 19):
-            return (False, 'The hotspot is offline.')
-        return (True, None)
+        return (True, 'Hotspot times seem to be not important for this script.')
+        # # Fine Bagels Hotspot on weekends:
+        # # Saturday only from 17:00, Sunday from 19:00 on
+        # now = datetime.datetime.now()
+        # if (now.weekday() == 5 and now.hour < 17) or \
+        #    (now.weekday() == 6 and now.hour < 19):
+        #     return (False, 'The hotspot is offline.')
+        # return (True, None)
